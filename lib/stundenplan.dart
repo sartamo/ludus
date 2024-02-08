c30a38d2ea66f4bed5da8fa39f01789d95c1d459
Timo <m1n3cr4f7undr0bl0x@gmail.com>
2024-02-08 22:01:02 +0100

    'Add Fach'-Button hinzugefügt (unfixed Bug: wenn faecher in faecherliste/stundenplan verändert wird, wird nicht der jewails ander screen geupdated)

--- lib/stundenplan.dart
+++ lib/stundenplan.dart
@@ -1,8 +1,9 @@
 // Seite für den Stundenplan
 
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/widgets.dart';
+import 'package:suppaapp/FaecherEinstellungen/hinzufuegen.dart';
 import 'package:suppaapp/globals.dart';
+import 'package:suppaapp/faecher.dart';
 
 class Stundenplan extends StatefulWidget {
   const Stundenplan({super.key});
@@ -22,12 +23,8 @@ class _StundenplanState extends State<Stundenplan> {
 
     List<List<String>> stundenplanA = List.generate(
         wochentage.length, (_) => List.filled(stunden.length, ''));
-    stundenplanA[0][0] = 'Mathe';
-    stundenplanA[1][1] = 'Deutsch';
-    stundenplanA[2][2] = 'Englisch';
-    stundenplanA[3][3] = 'Geschichte';
-    stundenplanA[4][4] = 'Kunst';
-    stundenplanA[1][0] = 'Mathe';
+
+    aktualisiereStundenplan(stundenplanA);
 
     // Define your list of Column widgets
     List<Widget> tage = List.generate((wochentage.length), (index) {
@@ -66,7 +63,7 @@ class _StundenplanState extends State<Stundenplan> {
                 width: breite,
                 child: CupertinoButton(
                   padding: EdgeInsets.zero,
-                  onPressed: () { },
+                  onPressed: () {},
                   color: hourColor,
                   disabledColor: hourColor,
                   pressedOpacity: 1.0,
@@ -80,46 +77,116 @@ class _StundenplanState extends State<Stundenplan> {
       );
     }).toList();
 
-    return SingleChildScrollView(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Column(
-            // First column (Side Bar)
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: hoehe,
-                width: breite,
-                child: const CupertinoButton(
-                  padding: EdgeInsets.zero,
-                  onPressed: null,
-                  color: firstColumnColor,
-                  disabledColor: firstColumnColor,
-                  pressedOpacity: 1.0,
-                  child: Text(''),
-                ),
+    return Padding(
+      padding: EdgeInsets.only(bottom: MediaQuery.of(context).padding.bottom),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            CupertinoNavigationBar(
+              middle: const Text('Stundenplan'),
+              trailing: CupertinoButton(
+                padding: EdgeInsets.zero,
+                child: const Icon(CupertinoIcons.settings),
+                onPressed: () {
+                  showCupertinoModalPopup(
+                    context: context,
+                    builder: (BuildContext context) => CupertinoActionSheet(
+                      title: const Text('Settings'),
+                      message: const Text('Select an option'),
+                      actions: <CupertinoActionSheetAction>[
+                        CupertinoActionSheetAction(
+                          child: const Text('Ändere den Stundenplan'),
+                          onPressed: () {
+                            Navigator.pop(context, 'Option 1');
+                            // Add your code for Option 1 here
+                          },
+                        ),
+                        CupertinoActionSheetAction(
+                          child: const Text('Füge Fach hinzu'),
+                          onPressed: () {
+                            result() async {
+                              Navigator.pop(context);
+                              return await Navigator.of(context).push(
+                                CupertinoPageRoute(
+                                    builder: (context) =>
+                                        const FachHinzufuegen()),
+                              );
+                            }
+
+                            result().then((output) {
+                              setState(() => faecher.add(output));
+                            });
+                          },
+                        ),
+                        // Add more options here
+                      ],
+                      cancelButton: CupertinoActionSheetAction(
+                        child: const Text('Cancel'),
+                        onPressed: () {
+                          Navigator.pop(context, 'Cancel');
+                        },
+                      ),
+                    ),
+                  );
+                },
               ),
-              ...stunden.map((stunde) {
-                return SizedBox(
-                  height: hoehe,
-                  width: breite,
-                  child: CupertinoButton(
-                    padding: EdgeInsets.zero,
-                    onPressed: null,
-                    color: firstColumnColor,
-                    disabledColor: firstColumnColor,
-                    pressedOpacity: 1.0,
-                    child: Text(stunde),
-                  ),
-                );
-              }).toList(),
-            ],
-          ),
-          ...tage
-        ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  // First column (Side Bar)
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: hoehe,
+                      width: breite,
+                      child: const CupertinoButton(
+                        padding: EdgeInsets.zero,
+                        onPressed: null,
+                        color: firstColumnColor,
+                        disabledColor: firstColumnColor,
+                        pressedOpacity: 1.0,
+                        child: Text(''),
+                      ),
+                    ),
+                    ...stunden.map((stunde) {
+                      return SizedBox(
+                        height: hoehe,
+                        width: breite,
+                        child: CupertinoButton(
+                          padding: EdgeInsets.zero,
+                          onPressed: null,
+                          color: firstColumnColor,
+                          disabledColor: firstColumnColor,
+                          pressedOpacity: 1.0,
+                          child: Text(stunde),
+                        ),
+                      );
+                    }).toList(),
+                  ],
+                ),
+                ...tage
+              ],
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  void aktualisiereStundenplan(List<List<String>> stundenplanA) {
+    for (int f = 0; f < faecher.length; f++) {
+      Fach myFach = faecher[f];
+      for (int w = 0; w < wochentage.length; w++) {
+        Set<int>? myZeiten = myFach.zeiten[w];
+        if (myZeiten != null) {
+          for (int s in myZeiten) {
+            stundenplanA[w][s] = myFach.name;
+          }
+        }
+      }
+    }
   }
 }
