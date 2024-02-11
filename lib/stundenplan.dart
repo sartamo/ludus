1fcccaa9b411eee7dc4ec4fb20198b087bbe90da
Timo <m1n3cr4f7undr0bl0x@gmail.com>
2024-02-11 10:28:39 +0100

    Stundenplan ändern-Knopf implementiert

--- lib/stundenplan.dart
+++ lib/stundenplan.dart
@@ -1,8 +1,11 @@
 // Seite für den Stundenplan
 
+//import 'dart:js_interop';
+//import 'dart:collection';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:suppaapp/FaecherEinstellungen/hinzufuegen.dart';
+//import 'package:suppaapp/faecherliste.dart';
 import 'package:suppaapp/globals.dart';
 import 'package:suppaapp/faecher.dart';
 
@@ -21,6 +24,8 @@ class _StundenplanState extends State<Stundenplan> {
       CupertinoColors.activeOrange; //Farbe der belegten Stunden
   final Color _freeColor =
       CupertinoColors.systemGrey2; //Farbe der freien Stunden
+  int _changingFach =
+      -1; //index des Fachs, von dem grade die Stunden geändert werden wenn -1 kein Fach wird geändert
 
   List<List<List<String>>> _stundenplanA = List.generate(
       //Äußere Liste: Wochentag, mittlere Liste: Stunde, innere Liste: Nammen der Fächer
@@ -53,7 +58,7 @@ class _StundenplanState extends State<Stundenplan> {
     if (_stundenplanA[d][h].isEmpty) {
       return CupertinoButton(
         padding: EdgeInsets.zero,
-        onPressed: null,
+        onPressed: _changingFach == -1 ? null : () => _clickButton(d: d, h: h, a: a), //if _changingFach == -1, onPressed is null, else onPressed is _clickButton
         color: _freeColor,
         disabledColor: _freeColor,
         pressedOpacity: 1.0,
@@ -62,11 +67,104 @@ class _StundenplanState extends State<Stundenplan> {
     } else {
       return CupertinoButton(
         padding: EdgeInsets.zero,
-        onPressed: () {},
+        onPressed: () {
+          _clickButton(d: d, h: h, a: a);
+        },
         color: _hourColor,
         disabledColor: _hourColor,
         pressedOpacity: 1.0,
         child: Text(_stundenplanA[d][h][a]),
+      );
+    }
+  }
+
+  void _clickButton({
+    required int d,
+    required int h,
+    required int a,
+  }) {
+    if (_changingFach == -1) {
+      int o = -1;
+
+      for (int f = 0; f < faecherList.faecher.length; f++) {
+        if (faecherList.faecher[f].zeiten[d]?.contains(h) ?? false) {
+          o++;
+          if (a == o) {
+            Navigator.of(context).push(
+              CupertinoPageRoute(builder: (context) => faecherList.faecher[f]),
+            );
+          }
+        }
+      }
+    } else {
+      Map<int, Set<int>> zeiten =
+          Map.from(faecherList.faecher[_changingFach].zeiten);
+      Fach currentFach = faecherList.faecher[_changingFach];
+      if (zeiten[d]?.contains(h) ?? false) {
+        Set<int> tempZeitenSet = Set.from(zeiten[d] ?? {});
+        tempZeitenSet.remove(h);
+        zeiten[d] = tempZeitenSet;
+        faecherList.updateFach(_changingFach, Fach(currentFach.name, zeiten));
+      } else {
+        if (zeiten[d] == null) {
+          zeiten[d] = {h};
+          faecherList.updateFach(_changingFach, Fach(currentFach.name, zeiten));
+        } else {
+          Set<int> tempZeitenSet = Set.from(zeiten[d] ?? {});
+          tempZeitenSet.add(h);
+          zeiten[d] = tempZeitenSet;
+          faecherList.updateFach(_changingFach, Fach(currentFach.name, zeiten));
+        }
+      }
+    }
+  }
+
+  Tooltip _changeFachButton() {
+    if (_changingFach == -1) {
+      return Tooltip(
+        message: 'Stundenplan ändern',
+        child: CupertinoButton(
+            padding: EdgeInsets.zero,
+            child: const Icon(CupertinoIcons.table_badge_more),
+            onPressed: () {
+              showCupertinoModalPopup(
+                  context: context,
+                  builder: ((context) => CupertinoActionSheet(
+                        title: const Text('Fach auswählen'),
+                        message: const Text(
+                            'Wähle ein Fach aus und dücke dann im Stundenplan auf die Stunden, an denen du das gewählte Fach hizufügen oder entfernen möchtest.'),
+                        actions: List.generate(faecherList.faecher.length, (f) {
+                          return CupertinoActionSheetAction(
+                            onPressed: () {
+                              setState(() {
+                                _changingFach = f;
+                                Navigator.pop(context);
+                              });
+                            },
+                            child: Text(faecherList.faecher[f].name),
+                          );
+                        }),
+                        cancelButton: CupertinoActionSheetAction(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text('Abbrechen'),
+                        ),
+                      )));
+            }),
+      );
+    } else {
+      return Tooltip(
+        message: 'Abbrechen',
+        child: CupertinoButton(
+          padding: EdgeInsets.zero,
+          child: const Icon(CupertinoIcons.clear),
+          onPressed: () {
+            setState(() {
+              _changingFach = -1;
+            });
+          },
+        ),
       );
     }
   }
@@ -147,16 +245,7 @@ class _StundenplanState extends State<Stundenplan> {
               trailing: Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  Tooltip(
-                    message: 'Stundenplan ändern',
-                    child: CupertinoButton(
-                      padding: EdgeInsets.zero,
-                      child: const Icon(CupertinoIcons.table_badge_more),
-                      onPressed: () {
-                        setState(() {});
-                      },
-                    ),
-                  ),
+                  _changeFachButton(),
                   Tooltip(
                     message: 'Fach hinzufügen',
                     child: CupertinoButton(
