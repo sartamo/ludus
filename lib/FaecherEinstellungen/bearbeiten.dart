4ca4a33c6d4dc407134fafb401a8db916491b275
Comedywaran <m1n3cr4f7undr0bl0x@gmail.com>
2024-04-09 22:18:43 +0200

    Slider für Farbe hinzugefügt (WIP)

--- lib/FaecherEinstellungen/bearbeiten.dart
+++ lib/FaecherEinstellungen/bearbeiten.dart
@@ -23,6 +23,17 @@ class _FachBearbeitenState extends State<FachBearbeiten> {
   late final SplayTreeMap<int, SplayTreeSet<int>> _zeiten =
       SplayTreeMap<int, SplayTreeSet<int>>.from(widget.fach.zeiten); // SplayTreeMap: Automatische Sortierung
   late final TextEditingController _textController;
+  final double _colorTextFieldWidth = (TextPainter(
+              text: const TextSpan(text: '999'),
+              textDirection: TextDirection.ltr)
+            ..layout())
+          .width+16;
+  int _colorRed = 0;
+  int _colorGreen = 0;
+  int _colorBlue = 0;
+  final TextEditingController _redController = TextEditingController();
+  final TextEditingController _greenController = TextEditingController();
+  final TextEditingController _blueController = TextEditingController();
 
   @override
   initState() {
@@ -85,12 +96,189 @@ class _FachBearbeitenState extends State<FachBearbeiten> {
                     _textController, // Fächername steht am Anfang im Textfeld
                 onChanged: (value) => setState(() => _selectedName = value),
               ),
+
+              
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      const SizedBox(
+                        width: 50,
+                        child: Text('Rot:'),
+                      ),
+                      SizedBox(
+                        width: _colorTextFieldWidth,
+                        child: CupertinoTextField(
+                          controller: _redController,
+                          autocorrect: false,
+                          keyboardType: TextInputType.number,
+                          maxLength: 3,
+                          textAlign: TextAlign.center,
+                          onChanged: (value) {
+                            try {
+                              setState(() {
+                                if (int.parse(value) <= 255) {
+                                  if (int.parse(value) >= 0) {
+                                    _colorRed = int.parse(value);
+                                  }
+                                  else
+                                  {
+                                    _redController.text = '0';
+                                    _colorRed = 0;
+                                  }
+                                }
+                                else {
+                                  _redController.text = '255';
+                                  _colorRed = 255;
+                                }
+                              });
+                            }
+                            catch (e) {
+                              _redController.text = _colorRed.toString();
+                            }
+                          },
+                        ),
+                      ),
+                      Expanded(
+                        child: CupertinoSlider(
+                          activeColor: CupertinoColors.systemRed,
+                          value: _colorRed.toDouble(),
+                          onChanged: (r) {
+                            setState(() {
+                              _colorRed = r.toInt();
+                              _redController.text = r.toInt().toString();
+                            });
+                          },
+                          min: 0,
+                          max: 255,
+                          thumbColor: Color.fromARGB(
+                              255, _colorRed, _colorGreen, _colorBlue),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const SizedBox(
+                        width: 50,
+                        child: Text('Grün:'),
+                      ),
+                      SizedBox(
+                        width: _colorTextFieldWidth,
+                        child: CupertinoTextField(
+                          controller: _greenController,
+                          autocorrect: false,
+                          keyboardType: TextInputType.number,
+                          maxLength: 3,
+                          textAlign: TextAlign.center,
+                          onChanged: (value) {
+                            try {
+                              setState(() {
+                                if (int.parse(value) <= 255) {
+                                  if (int.parse(value) >= 0) {
+                                    _colorGreen = int.parse(value);
+                                  }
+                                  else
+                                  {
+                                    _greenController.text = '0';
+                                    _colorGreen = 0;
+                                  }
+                                }
+                                else {
+                                  _greenController.text = '255';
+                                  _colorGreen = 255;
+                                }
+                              });
+                            }
+                            catch (e) {
+                              _greenController.text = _colorGreen.toString();
+                            }
+                          },
+                        ),
+                      ),
+                      Expanded(
+                        child: CupertinoSlider(
+                          activeColor: CupertinoColors.systemGreen,
+                          value: _colorGreen.toDouble(),
+                          onChanged: (r) {
+                            setState(() {
+                              _colorGreen = r.toInt();
+                              _greenController.text = r.toInt().toString();
+                            });
+                          },
+                          min: 0,
+                          max: 255,
+                          thumbColor: Color.fromARGB(
+                              255, _colorRed, _colorGreen, _colorBlue),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const SizedBox(
+                        width: 50,
+                        child: Text('Blau:'),
+                      ),
+                      SizedBox(
+                        width: _colorTextFieldWidth,
+                        child: CupertinoTextField(
+                          controller: _blueController,
+                          autocorrect: false,
+                          keyboardType: TextInputType.number,
+                          maxLength: 3,
+                          textAlign: TextAlign.center,
+                          onChanged: (value) {
+                            try {
+                              setState(() {
+                                if (int.parse(value) <= 255) {
+                                  if (int.parse(value) >= 0) {
+                                    _colorBlue = int.parse(value);
+                                  }
+                                  else
+                                  {
+                                    _blueController.text = '0';
+                                    _colorBlue = 0;
+                                  }
+                                }
+                                else {
+                                  _blueController.text = '255';
+                                  _colorBlue = 255;
+                                }
+                              });
+                            }
+                            catch (e) {
+                              _blueController.text = _colorBlue.toString();
+                            }
+                          },
+                        ),
+                      ),
+                      Expanded(
+                        child: CupertinoSlider(
+                          activeColor: CupertinoColors.systemBlue,
+                          value: _colorBlue.toDouble(),
+                          onChanged: (r) {
+                            setState(() {
+                              _colorBlue = r.toInt();
+                              _blueController.text = r.toInt().toString();
+                            });
+                          },
+                          min: 0,
+                          max: 255,
+                          thumbColor: Color.fromARGB(
+                              255, _colorRed, _colorGreen, _colorBlue),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+
               StundenplanBearbeiten(
                 zeiten: _zeiten,
                 name: _selectedName,
                 currentFachIndex: _searchFach(),
               ),
-
             ],
           ),
         ),
